8ff59c605801ffeb74800bf0c530d253a95c2207
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-11-20 11:09:47 +0530

    Add when we presses Enter

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 //Get HTML elements
 const newTodoInput = document.getElementById("new-todo-input");
 const todoList = document.getElementById("todo-list");
+const todos = []; // This array store all our todos.
+
+//Add todo when user presses Enter
+
+newTodoInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    const text = newTodoInput.value.trim(); //remove extra spaces
+
+    if (text === "") {
+      return;
+    }
+
+    addTodo(text);
+    newTodoInput.value = "";
+  }
+});
